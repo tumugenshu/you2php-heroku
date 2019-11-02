cc503f707210920880d76a60b3e49ffef768f165
wg yang <37625051+tumugenshu@users.noreply.github.com>
2019-11-02 11:47:48 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -3,7 +3,7 @@ $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyBHEu0R4RCaqohqqN2DsNC__mLpWAy3guQ');
 define('GJ_CODE', $gl);
-define('SITE_NAME', 'yangwenwechat84');
+define('SITE_NAME', 'yangwenwechat84.in');
 define('TITLENAME', 'Hello wechat!');
 define('EN2DEKEY', 'AIzaSyBmYQPhWRuVcFG8XNO');
 define('EMAIL', 'wengenyoung@gmail.com');
